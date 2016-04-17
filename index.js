631c11fbdfcf22d4a8f94f86d6c45beddfb5f4f4
Jermaine Oppong <jeropp00@gmail.com>
2016-04-17 23:16:24 +0100

    Refactors implementation. Adds documentation.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var globalSecret = 'simple secret'; // Should be dynamically generated
 // Middleware settings
 app.use(bodyParser.urlencoded({extended: false})); // create application/x-www-form-urlencoded parser
 app.use(expressJwt({secret: globalSecret}).unless({
-    path: [
+    path: [ // Does not validate token is paths match any of these routes
         '/', 
         '/login'
     ]
@@ -28,18 +28,22 @@ app.get('/', function(req, res) {
 
 app.post('/login', function(req, res, next) {
     var userAuth = req.body;
-    // Simulate connection to database
+    
+    // TODO: Connect to database rather than in-memory object
     if(userAuth.username === mockDatabase.user &&
         userAuth.password === mockDatabase.pass) {
+        
+        // creates a json web token to distribute as logged in bearer
         var responseToken = jwt.sign({user: userAuth.username}, globalSecret);
-        console.log(responseToken);
+        
+        // console.log(responseToken);
         res.status(200).json({auth: responseToken});
     } else {
         res.status(401).send('Invalid username or password!');
     }
 });
 
-app.post('/protected', function(req, res) {
+app.get('/protected', function(req, res) {
     res.send('Viewing protected route!');
 });
 
