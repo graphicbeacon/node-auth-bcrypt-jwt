277fb2822df51b31b7fb346c2ed180cbc95383f8
Jermaine Oppong <jeropp00@gmail.com>
2016-04-30 19:05:02 +0100

    Code refactor. Abstracts routing into external module.

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ var express = require('express');
 var auth = require('./server/lib/auth.service');
 var store = require('./server/lib/store.service.js');
 var middleware = require('./server/lib/middleware.js');
+var routes = require('./server/lib/routes.js');
 
 
 // ----------------------------
 // Config
 // ----------------------------
 var config = {
+    port: 4000,
     secret: 'simple secret',  // TODO: Dynamically generate secret key
     paths: {
         tokenWhitelist: ['/', '/login', '/signup'],
@@ -23,6 +25,7 @@ var config = {
 // ----------------------------
 var app = express(); // Start Express instance
 
+
 // ----------------------------
 // Middleware settings
 // ----------------------------
@@ -35,45 +38,17 @@ middleware.init({
 // ----------------------------
 // Routes
 // ----------------------------
-app.get('/', function(req, res) {
-    res.render('index', {viewTitle: 'Hello World', content: 'Hello World'});
+routes.init({
+    app: app,
+    auth: auth,
+    store: store,
+    secret: config.secret
 });
-
-app.get('/login', function(req, res) {
-    // TODO: Redirect to protected screen if already logged in
-    res.render('login', {viewTitle: 'Login', content: 'Please login'});
-});
-
-app.get('/signup', function(req, res) {
-    // TODO: Redirect to protected screen if already logged in
-    res.render('signup', {viewTitle: 'Signup', content: 'Sign up'});
-});
-
-app.get('/protected', function(req, res) {
-    res.render('protected', {viewTitle: 'Protected', content: 'User Dashboard'});
-});
-
-app.get('/logout', function(req, res) {
-    // TODO: check that referrer and origin are the same domain to prevent cross site attacks
-    // Remove auth cookie and redirect to login again
-    res.clearCookie('auth');
-    res.status(302).redirect('/login'); 
-});
-
-app.post('/login', auth.login({
-    redirectTo: '/protected',
-    secret: config.secret,
-    store: store
-}));
-
-app.post('/signup', auth.signup({
-    store: store
-}));
 
 
 // ----------------------------
 // App start
 // ----------------------------
-app.listen(4000, function() {
-    console.log('App listening on port 4000...');
+app.listen(config.port, function() {
+    console.log('App listening on port ' + config.port + '...');
 });
---
+++ server/lib/routes.js
@@ -0,0 +1,41 @@
+module.exports.init = function(config) {
+    var app = config.app,
+        auth = config.auth,
+        store = config.store,
+        secret = config.secret;
+        
+    app.get('/', function(req, res) {
+        res.render('index', {viewTitle: 'Hello World', content: 'Hello World'});
+    });
+
+    app.get('/login', function(req, res) {
+        // TODO: Redirect to protected screen if already logged in
+        res.render('login', {viewTitle: 'Login', content: 'Please login'});
+    });
+
+    app.get('/signup', function(req, res) {
+        // TODO: Redirect to protected screen if already logged in
+        res.render('signup', {viewTitle: 'Signup', content: 'Sign up'});
+    });
+
+    app.get('/protected', function(req, res) {
+        res.render('protected', {viewTitle: 'Protected', content: 'User Dashboard'});
+    });
+
+    app.get('/logout', function(req, res) {
+        // TODO: check that referrer and origin are the same domain to prevent cross site attacks
+        // Remove auth cookie and redirect to login again
+        res.clearCookie('auth');
+        res.status(302).redirect('/login'); 
+    });
+
+    app.post('/login', auth.login({
+        redirectTo: '/protected',
+        secret: secret,
+        store: store
+    }));
+
+    app.post('/signup', auth.signup({
+        store: store
+    }));
+}
